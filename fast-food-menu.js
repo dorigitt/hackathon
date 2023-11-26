8984c7cbed45cd2b505ec64843af80959f2a0fb2
Dariga Qazaq <darigaqazaq@MacBook-Air-Dariga.local>
2023-11-26 10:53:27 +0600

    Initial commit

---
+++ fast-food-menu.js
@@ -0,0 +1,57 @@
+function buttonEdit(event) {
+    const clickedButton = event.target;
+
+    if (clickedButton.textContent.trim() === 'Выбрать') {
+        clickedButton.textContent = 'В КОРЗИНЕ';
+    }
+}
+
+const buttons = document.querySelectorAll('.action');
+buttons.forEach(button => {
+    button.addEventListener('click', buttonEdit);
+});
+function buttonEdit(event) {
+    const clickedButton = event.target;
+
+    clickedButton.textContent =
+        clickedButton.textContent.trim() === 'Выбрать' ? 'В КОРЗИНЕ' : 'Выбрать';
+}
+
+const buttons2 = document.querySelectorAll('.action');
+buttons2.forEach(button => {
+    button.addEventListener('click', buttonEdit);
+});
+
+let totalPrice = 0;
+
+function addToTotal(price) {
+    totalPrice += price;
+    updateTotalDisplay();
+}
+
+function updateTotalDisplay() {
+    const totalElement = document.getElementById('total');
+    totalElement.textContent = totalPrice;
+}
+
+function clearTotal() {
+    totalPrice = 0;
+    updateTotalDisplay();
+}
+const clearButton = document.getElementById('clearButton');
+clearButton.addEventListener('click', clearTotal);
+
+function fillGaps() {
+    if (document.getElementById('name').value === "") {
+        alert("Input your name");
+    } else if (document.getElementById('phone').value === "") {
+        alert("Input your number");
+    } else if (document.getElementById('car').value === "") {
+        alert("Input the place");
+    } else {
+        alert("Success payment,Thank you for the purchase");
+    }
+}
+
+const table = document.getElementById("price-button");
+table.addEventListener('click', fillGaps);
---
+++ script.js
@@ -0,0 +1,73 @@
+function showOAvtoPark() {
+    document.getElementById('order').scrollIntoView({ behavior: "smooth" });
+}
+
+const show = document.getElementById('main-button');
+show.addEventListener('click', showAvtoPark);
+
+const buttons = document.querySelectorAll('.order-button');
+for (let i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById('price').scrollIntoView({ behavior: "smooth" });
+    }
+}
+
+function fillGaps() {
+    if (document.getElementById('name').value === "") {
+        alert("Input your name");
+    } else if (document.getElementById('phone').value === "") {
+        alert("Input your number");
+    } else if (document.getElementById('car').value === "") {
+        alert("Input the place");
+    } else {
+        alert("Thank you, we'll contact with you soon");
+    }
+}
+
+const table = document.getElementById("price-button");
+table.addEventListener('click', fillGaps);
+
+
+
+
+function sendReview() {
+    
+    var reviewText = document.getElementById('txt').value;
+
+   
+    if (reviewText.trim() !== '') {
+        
+        alert('Review is sent');
+
+        
+        document.querySelector('.cust-review').value = '';
+    } else {
+       
+        alert('Please enter a review before sending.');
+    }
+}
+
+function redirectToAnotherPage1() {
+    
+    var newPageUrl = "ashana-menu.html";
+    
+    
+    window.location.href = newPageUrl;
+}
+
+function redirectToAnotherPage2() {
+    
+    var newPageUrl = "red-canteen-menu.html";
+    
+    
+    window.location.href = newPageUrl;
+}
+
+function redirectToAnotherPage3() {
+    
+    var newPageUrl = "fast-food-menu.html";
+    
+    
+    window.location.href = newPageUrl;
+}
+
